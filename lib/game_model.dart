951e695c22baf5652c3d252784651b6df031897f
AustinRice <fkarice@gmail.com>
2023-02-08 15:45:30 -0500

    Constructor for random color at start

--- lib/game_model.dart
+++ lib/game_model.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:fish_bowl_game/countdown_timer.dart';
 import 'package:fish_bowl_game/game_screen.dart';
 import 'package:fish_bowl_game/round_results_screen.dart';
@@ -11,12 +13,25 @@ class GameModel extends ChangeNotifier {
   List<int> _team1Score = [0, 0, 0];
   List<int> _team2Score = [0, 0, 0];
   bool _team1Turn = true;
+  int _gameColorIndex = 0;
+  final List<Color> _gameColors = [
+    Colors.red,
+    Colors.blue,
+    Colors.pink,
+    Colors.purple,
+    Colors.orange,
+  ];
+
+  GameModel() {
+    _gameColorIndex = Random().nextInt(_gameColors.length);
+  }
 
   String get currentThing => thingsLeft > 0 ? _wordsInRound.first : "EMPTY";
   int get thingsLeft => _wordsInRound.length;
   int get thingCount => _words.length;
   String get team => _team1Turn ? "Team 1" : "Team 2";
   String get round => "Round ${_round + 1}";
+  Color get gameColor => _gameColors[_gameColorIndex];
 
   String get rules {
     switch (_round) {
@@ -33,17 +48,6 @@ class GameModel extends ChangeNotifier {
   int roundScore(bool team1, int round) {
     return team1 ? _team1Score[round] : _team2Score[round];
   }
-
-  final List<Color> _gameColors = [
-    Colors.red,
-    Colors.blue,
-    Colors.pink,
-    Colors.purple,
-    Colors.orange,
-  ];
-
-  int _gameColorIndex = 0;
-  Color get gameColor => _gameColors[_gameColorIndex];
 
   void nextColor() {
     _gameColorIndex++;
