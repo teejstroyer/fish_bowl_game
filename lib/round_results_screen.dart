d16911acaf40d0c4806fb48fce38905a4da0551e
Titoriano Broadus <titoriano@gmail.com>
2023-02-16 22:19:50 -0600

    current working version of scoreboard

--- lib/round_results_screen.dart
+++ lib/round_results_screen.dart
@@ -1,5 +1,6 @@
 import 'package:fish_bowl_game/countdown_timer.dart';
 import 'package:fish_bowl_game/game_model.dart';
+import 'package:fish_bowl_game/scoreboard.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -23,15 +24,32 @@ class RoundResultsScreeen extends StatelessWidget {
         child: SafeArea(
           child: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text(gameModel.round),
-                Text(roundMessage),
-                scoreBoard(gameModel),
-                const Text("DA RULES"),
-                Text(gameModel.rules),
                 Expanded(
                   child: Column(
                     children: [
+                      Text(
+                        gameModel.round,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      Text(roundMessage),
+                    ],
+                  ),
+                ),
+                Expanded(child: ScoreBoard(model: gameModel)),
+                Expanded(
+                  child: Column(
+                    children: [
+                      const Text("DA RULES"),
+                      Text(gameModel.rules),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(gameModel.team),
                       TextButton(
                         onPressed: () {
                           gameModel.showGameScreen(context);
@@ -46,65 +64,6 @@ class RoundResultsScreeen extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-
-  Row scoreBoard(GameModel model) {
-    int team1Total = 0;
-    int team2Total = 0;
-
-    List<Widget> scores = [];
-
-    scores.add(Column(children: const [
-      Text("Team 1"),
-      Text("Team 2"),
-    ]));
-
-    scores.add(const VerticalDivider(
-      width: 10,
-      thickness: 1,
-      indent: 2,
-      endIndent: 2,
-      color: Colors.white,
-    ));
-
-    for (int i = 0; i < 3; i++) {
-      int t1 = model.roundScore(true, i);
-      int t2 = model.roundScore(false, i);
-
-      team1Total += t1;
-      team2Total += t2;
-
-      scores.add(Column(children: [
-        Text(t1.toString()),
-        Text(t2.toString()),
-      ]));
-
-      scores.add(const VerticalDivider(
-        width: 5,
-        thickness: 1,
-        indent: 2,
-        endIndent: 2,
-        color: Colors.transparent,
-      ));
-    }
-
-    scores.add(const VerticalDivider(
-      width: 10,
-      thickness: 1,
-      indent: 2,
-      endIndent: 2,
-      color: Colors.white,
-    ));
-
-    scores.add(Column(children: [
-      Text(team1Total.toString()),
-      Text(team2Total.toString()),
-    ]));
-
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: scores,
     );
   }
 }
